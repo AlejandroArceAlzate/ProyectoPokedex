9e5cda85f12b08fdefc856e822b4c9a3faacb1dd
DarKray33 <alejandroarcealzate@hotmail.com>
2014-11-27 13:13:03 -0500

    añadiendo bootstrap
    se añadio bootstrap, para mejorar el diseño de la aplicacion

--- public/js/app.js
+++ public/js/app.js
@@ -7,8 +7,8 @@
 			name: "Bulbasaur",
 			species: "Seed Pokemon",
 			type: ["Grass", "Poison"],
-			height: "2´4",
-			weight: "15.2 lbs",
+			height: "2′4″ (0.71m)",
+			weight: "15.2 lbs (6.9 kg)",
 			abilities: ["Overgrow", "Chlorophyll"]
 		};
 	})
