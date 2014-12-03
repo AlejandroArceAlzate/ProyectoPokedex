629210b4e93e79ec0a3f53175e8973bbfa361338
DarKray33 <alejandroarcealzate@hotmail.com>
2014-12-03 10:54:23 -0500

    ngRoute y vista para pokedex y pokemon
    creacion de las vistas de pokemon y pokedex y creacion de las rutas
    basicas para la aplicacion

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,22 @@
 (function (){
-	angular.module("pokedex", [
+	var app = angular.module("pokedex", [
+		'ngRoute',
 		'pokedex.controllers',
 	    'pokedex.directives',
 	    'pokedex.filters'
 		]);
-
+	app.config(['$routeProvider', function($routeProvider){
+		$routeProvider
+			.when('/',{
+				templateUrl: 'views/pokedex.html'
+			})
+			.when('/pokemon/:id',{
+				templateUrl: 'views/pokemon.html',
+				controller: 'PokemonController',
+				controllerAs: 'pkmCtrl'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+	}]);
 })();
